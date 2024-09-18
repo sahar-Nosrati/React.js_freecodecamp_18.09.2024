bde70a637664c21b895295f50dca5543d8a08aa6
sahar-Nosrati <sahar.nosrati.shanjani@gmail.com>
2024-09-18 22:44:05 +0200

    component

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="../img/reactlogo.png" alt="react_log" />
+        </nav>
+      </header>
+      <h1> Why React.js is important!</h1>
+      <ul>
+        <li>It is nice to learn </li>
+        <li>Improve my programming skills</li>
+        <li>A kind of challange for me and I like it</li>
+      </ul>
+      <footer>©20xx Nosrati. All rights reserved</footer>
+    </div>
+  );
+}
+export default App;
